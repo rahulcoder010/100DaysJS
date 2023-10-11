0b3a949d8d7e6335fa47640ea864ca5868671bf5
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-10-11 12:29:22 +0530

    PB-2 (task) dddd

---
+++ test.js
@@ -0,0 +1,15 @@
+const testJSContents = `
+// Existing code in test.js
+// ...
+`
+
+const updatedJSContents = `
+// Existing code in test.js
+// ...
+
+// Write test cases for login
+// ...
+`
+
+// Returning the updated file contents in the same order
+testJSContents + updatedJSContents
