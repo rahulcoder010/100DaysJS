868cb798543efcb8725af5d0ffda1919dae859d9
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-10-20 17:01:09 +0530

    PB-8 (task) Issue8

---
+++ siguptestcases.js
@@ -0,0 +1,23 @@
+const siguptestcases = `
+const registrationTestCases = [
+  {
+    email: 'user1@example.com',
+    password: 'password1',
+    mobileno: '1234567890'
+  },
+  {
+    email: 'user2@example.com',
+    password: 'password2',
+    mobileno: '9876543210'
+  },
+  {
+    email: 'user3@example.com',
+    password: 'password3',
+    mobileno: '5555555555'
+  }
+];
+
+module.exports = registrationTestCases;
+`;
+
+siguptestcases
