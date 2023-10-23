33ccab112202f8f9e09a4f978667906ca2c49d42
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-10-23 11:06:48 +0530

    PB-1 (task) test

---
+++ test.js
@@ -0,0 +1,23 @@
+`// test.js
+
+// test cases for login
+describe("Login Test Cases", () => {
+  it("should pass when correct username and password are provided", () => {
+    // write your test case here
+  });
+  
+  it("should fail when incorrect username and password are provided", () => {
+    // write your test case here
+  });
+});
+
+// test cases for registration
+describe("Registration Test Cases", () => {
+  it("should pass when valid username and password are provided", () => {
+    // write your test case here
+  });
+  
+  it("should fail when username or password is missing", () => {
+    // write your test case here
+  });
+});`
