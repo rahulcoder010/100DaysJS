9492c3478d900928dd939c160ea9a93cd5ddc065
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-10-23 17:11:24 +0530

    PB-1 (task) test

---
+++ test.js
@@ -0,0 +1,21 @@
+let testContent = `
+// Test cases for login
+test('Login with valid credentials', () => {
+  // Implement test logic here
+});
+
+test('Login with invalid credentials', () => {
+  // Implement test logic here
+});
+
+// Test cases for registration
+test('Registration with valid data', () => {
+  // Implement test logic here
+});
+
+test('Registration with incomplete data', () => {
+  // Implement test logic here
+});
+`;
+
+testContent;
