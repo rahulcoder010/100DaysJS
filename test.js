c4cbc87570e515f9894a610ca051b5a33bb9f8fa
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-10-23 12:07:52 +0530

    PB-1 (task) test

---
+++ test.js
@@ -0,0 +1,25 @@
+```javascript
+// test.js
+
+// Login test cases
+describe("Login", () => {
+  it("should successfully log in with valid credentials", () => {
+    // Test code here
+  });
+
+  it("should display an error message for invalid credentials", () => {
+    // Test code here
+  });
+});
+
+// Registration test cases
+describe("Registration", () => {
+  it("should successfully register a new user", () => {
+    // Test code here
+  });
+
+  it("should display an error message for existing username", () => {
+    // Test code here
+  });
+});
+```
